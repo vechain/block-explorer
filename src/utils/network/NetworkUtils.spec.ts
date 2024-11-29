8add66de459d6f9350d712068767129c0c2e38f0
Daithi Hearn <daithi.hearn@gmail.com>
2024-11-29 16:25:24 +0100

    fix unit tests

---
+++ src/utils/network/NetworkUtils.spec.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest"
+import { getNetworkByUrl } from "./NetworkUtils"
+import { VALID_NETWORKS } from "@/constants/network/NetworkConst"
+
+describe("getNetworkByUrl", () => {
+  it("should return the correct network object when a valid URL is provided", () => {
+    const url = VALID_NETWORKS[0].url
+    const result = getNetworkByUrl(url)
+    expect(result).toEqual(VALID_NETWORKS[0])
+  })
+
+  it("should return undefined when an invalid URL is provided", () => {
+    const url = "https://invalid.url"
+    const result = getNetworkByUrl(url)
+    expect(result).toBeUndefined()
+  })
+})
