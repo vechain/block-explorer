bcb607d13ba8367f82cfdd5c56b906b488a775d7
DavNej <david.nejar@gmail.com>
2025-07-09 11:40:13 +0200

    create getB3trRewardDistributedEvents action

---
+++ src/actions/getB3trRewardDistributedEvents.ts
@@ -0,0 +1,40 @@
+import { RewardDistributedEventSignature } from "@/constants/vebetterdao/EventSignatures.ts"
+import { EventLogs, ThorClient } from "@vechain/sdk-network"
+import { Network } from "@/constants/network"
+
+export type GetB3trRewardDistributedEventsReturnType = EventLogs[]
+
+export async function getB3trRewardDistributedEvents({
+  thorClient,
+  network,
+  fromBlock,
+  toBlock,
+}: {
+  thorClient: ThorClient
+  network: Network
+  fromBlock: number
+  toBlock: number
+}): Promise<GetB3trRewardDistributedEventsReturnType> {
+  const X2EarnRewardsPoolAddress = network.contracts.X2EarnRewardsPool
+
+  if (!fromBlock || !toBlock || !X2EarnRewardsPoolAddress) {
+    return []
+  }
+
+  const result = await thorClient.logs.filterRawEventLogs({
+    range: {
+      unit: "block",
+      from: fromBlock,
+      to: toBlock,
+    },
+    criteriaSet: [
+      {
+        address: X2EarnRewardsPoolAddress,
+        topic0: RewardDistributedEventSignature,
+      },
+    ],
+    order: "desc",
+  })
+
+  return result
+}
