c8fd43f0c660e94a36e1e98d5605f2e854b81faf
DavNej <david.nejar@gmail.com>
2025-07-09 11:09:11 +0200

    create getAccount action

---
+++ src/actions/getAccount.ts
@@ -0,0 +1,26 @@
+import { Address } from "@vechain/sdk-core"
+import { AccountDetail, ThorClient } from "@vechain/sdk-network"
+
+export type GetAccountReturnType = {
+  address: string
+  details: AccountDetail
+}
+
+export async function getAccount({
+  thorClient,
+  address,
+}: {
+  thorClient: ThorClient
+  address: Address
+}): Promise<GetAccountReturnType | null> {
+  const accountDetails = await thorClient.accounts.getAccount(address)
+
+  if (!accountDetails) return null
+
+  const result = {
+    address: address.toString(),
+    details: accountDetails,
+  } satisfies GetAccountReturnType
+
+  return result
+}
