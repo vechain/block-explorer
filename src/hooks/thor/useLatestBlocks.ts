b9390d3d57db6514693ddd0a48d892e5168efa40
DavNej <david.nejar@gmail.com>
2025-07-09 16:45:47 +0200

    create useLatestBlock hook

---
+++ src/hooks/thor/useLatestBlocks.ts
@@ -0,0 +1,25 @@
+import { useQueries } from "@tanstack/react-query"
+import { useThorClient } from "@/hooks/useThorClient"
+import { getBlock } from "@/actions/getBlock"
+import { Revision } from "@vechain/sdk-core"
+import { useBestBlock } from "./useBestBlock"
+
+export function useLatestBlocks({ count }: { count: number }) {
+  const { thorClient } = useThorClient()
+  const { data: bestBlock } = useBestBlock()
+
+  const bestBlockNumber = bestBlock?.number ?? count
+  const blockIds = []
+
+  for (let i = 1; i < count; i++) {
+    blockIds.push(bestBlockNumber - i)
+  }
+
+  return useQueries({
+    queries: blockIds.map(blockId => ({
+      queryKey: [getBlock.name, blockId],
+      queryFn: () => getBlock({ thorClient, revision: Revision.of(blockId) }),
+      staleTime: Infinity,
+    })),
+  })
+}
