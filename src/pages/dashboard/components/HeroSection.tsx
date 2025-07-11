1da81e04bad4318d90d30d4f4c88c8d840bcb370
DavNej <david.nejar@gmail.com>
2025-07-11 14:58:15 +0200

    hero section according to design

--- src/pages/dashboard/components/HeroSection.tsx
+++ src/pages/dashboard/components/HeroSection.tsx
@@ -1,55 +1,24 @@
-import { Box, Text, VStack, Flex, Icon } from "@chakra-ui/react"
-import { LuArrowBigRight, LuBox } from "react-icons/lu"
-import { FaLinode } from "react-icons/fa"
-import InsightData from "./InsightDataCard"
-import { SearchBar } from "@/components/search/SearchBar"
-import { useBestBlock } from "@/hooks/thor/useBestBlock"
+import { Text, Flex, Stack } from "@chakra-ui/react"
+import { BlockNumberCard, TotalTransactionsCard, ValidatorNodesCard } from "./InsightDataCard"
 import { useTranslation } from "react-i18next"
+import { SearchBar } from "@/components/search/SearchBar"
 
-export const BlockchainInsightsComponent = () => {
-  const { data: bestBlock, isLoading } = useBestBlock()
+export const HeroSection = () => {
   const { t } = useTranslation()
 
   return (
-    <Flex w="100%" m={4} justifyContent="center">
-      <Flex w="80%" borderRadius="24px" bg="grey.700" gap={6} p={12}>
-        <VStack flex={8} justify={"center"}>
-          <Text textAlign={"left"} w={"100%"} fontSize="18px" color={"grey.200"} fontWeight={600}>
-            {t("tracking_blockchain_insights")}
-          </Text>
-          <SearchBar />
-        </VStack>
-        <Box flex="4" textAlign="center">
-          <InsightData
-            icon={
-              <Icon mr={1} size={"md"} color={"grey.400"}>
-                <LuBox />
-              </Icon>
-            }
-            label={t("block_number")}
-            value={bestBlock?.number.toLocaleString()}
-            loading={isLoading}
-          />
-          <InsightData
-            icon={
-              <Icon mr={1} color={"grey.400"} size={"md"}>
-                <LuArrowBigRight />
-              </Icon>
-            }
-            label={t("total_transactions")}
-            value={"-----"}
-          />
-          <InsightData
-            icon={
-              <Icon mr={2} size={"md"} color={"grey.400"}>
-                <FaLinode />
-              </Icon>
-            }
-            label={t("validator_nodes")}
-            value={"-----"}
-          />
-        </Box>
+    <Stack py={16} px={40} bg='bg.subtle' rounded="xl" gap={24}>
+      <Text maxW="700px" fontSize="5xl" fontWeight={600}>
+        {t("tracking_blockchain_insights")}
+      </Text>
+
+      <SearchBar />
+
+      <Flex gap={4}>
+        <BlockNumberCard />
+        <TotalTransactionsCard />
+        <ValidatorNodesCard />
       </Flex>
-    </Flex>
+    </Stack>
   )
 }
--- src/pages/dashboard/components/InsightDataCard.tsx
+++ src/pages/dashboard/components/InsightDataCard.tsx
@@ -1,48 +1,76 @@
-import { Box, Flex, Text, Skeleton } from "@chakra-ui/react"
+import { useBestBlock } from "@/hooks/thor/useBestBlock"
+import { Flex, Text, Skeleton, Stack, Icon, Heading } from "@chakra-ui/react"
+import { useTranslation } from "react-i18next"
+import { LuArrowLeftRight, LuBox, LuUserCheck } from "react-icons/lu"
 
-type StatCardProps = {
-  /**
-   * An icon element to be displayed alongside the label.
-   * Example: A React component representing an SVG or icon, such as <Icon />.
-   */
-  icon: React.ReactElement
+export const BlockNumberCard = () => {
+  const { t } = useTranslation()
+  const { data: bestBlock, isPending } = useBestBlock()
 
-  /**
-   * A string representing the label for the data being displayed.
-   * Example: "Block Number", "Total Transactions".
-   */
-  label: string
+  const blockNumber = bestBlock?.number.toLocaleString() || "-"
 
-  /**
-   * The value to display, which can be any React node (e.g., a string, number, or React element).
-   * Example: "123,456", <Skeleton />, or custom elements.
-   * Default: `undefined` (no value displayed).
-   */
-  value?: React.ReactNode
-
-  /**
-   * A boolean indicating whether the data is in a loading state.
-   * If true, a skeleton loader is displayed instead of the value.
-   * Example: `true` or `false`.
-   * Default: `false` (no loading skeleton is shown).
-   */
-  loading?: boolean
+  return <InsightDataCard label={t("block_number")} value={blockNumber} isPending={isPending} icon={<LuBox />} />
 }
 
-const InsightDataCard = ({ icon, label, value, loading }: StatCardProps) => {
+export const TotalTransactionsCard = () => {
+  const { t } = useTranslation()
+  const { isPending } = useBestBlock()
+
+  const totalTransactions = Number(123456789).toLocaleString()
+
   return (
-    <Box bg="grey.600" borderRadius="md" m={2} display="flex" justifyContent="space-between">
-      <Flex gap={3} p={4} w={"100%"} justifyContent={"space-between"}>
-        <Text color="grey.300" fontWeight="bold" display="flex" alignItems="center" gap={2}>
-          {icon}
-          {label}
-        </Text>
-        <Box textAlign="right" color="grey.300" fontWeight="bold">
-          {loading ? <Skeleton height="20px" width="100px" /> : value}
-        </Box>
-      </Flex>
-    </Box>
+    <InsightDataCard
+      label={t("total_transactions")}
+      value={totalTransactions}
+      isPending={isPending}
+      icon={<LuArrowLeftRight />}
+    />
   )
 }
 
-export default InsightDataCard
+export const ValidatorNodesCard = () => {
+  const { t } = useTranslation()
+  const { isPending } = useBestBlock()
+
+  const validatorNodes = Number(12345).toLocaleString()
+
+  return (
+    <InsightDataCard label={t("validator_nodes")} value={validatorNodes} isPending={isPending} icon={<LuUserCheck />} />
+  )
+}
+
+type InsightDataCardProps = {
+  label: string
+  value: string
+  isPending: boolean
+  icon: React.ReactElement
+}
+
+const InsightDataCard = ({ icon, label, value, isPending }: InsightDataCardProps) => {
+  const circle = { rounded: "full", border: "1px solid", borderColor: "border.emphasized", p: 1 }
+
+  return (
+    <Flex gap={6} borderRadius="xl" px={6} py={10} bg="bg.emphasized" flex={1}>
+      <Stack flex={1}>
+        <Heading as="h4" size="xs" fontWeight="bold" color="fg.muted" textTransform="uppercase">
+          {label}
+        </Heading>
+        {isPending  ? (
+          <Skeleton height="30px" width="80%" bg="fg.muted" />
+        ) : (
+          <Text fontSize="2xl" fontWeight="bold" color="fg">
+            {value}
+          </Text>
+        )}
+      </Stack>
+
+      <Flex w="60px" h="60px" alignItems="center" justifyContent="center" {...circle}>
+        <Flex w="100%" h="100%" alignItems="center" justifyContent="center" {...circle}>
+          <Icon w="20px" h="20px" color="fg">
+            {icon}
+          </Icon>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
