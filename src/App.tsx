e43f1f58f2a2c481322ff466ce5350482d98d31f
DavNej <david.nejar@gmail.com>
2025-07-14 15:40:17 +0200

    separate App into Router and Provider (#53)
    * create getAccount action
    * create getVnsName action
    * create getVnsAddress action
    * create getBlock action
    * create getTransaction action
    * create getB3trRewardDistributedEvents action
    * add THOR_SOLO_URL from sdk in network
    * update actions/upload-artifact@v3 to v4
    * separate App into Router and Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { NetworkProvider } from "@/providers/NetworkProvider.tsx"
 import Dashboard from "@/pages/Dashboard.tsx"
@@ -10,29 +10,46 @@ import AccountPage from "@/pages/AccountPage.tsx"
 import Navbar from "@/components/navigation/NavBar.tsx"
 import { Box } from "@chakra-ui/react"
 
+import { ChakraProvider } from "@chakra-ui/react"
+import { ColorModeProvider } from "@/components/ui/ColorMode.tsx"
+import { config } from "@/components/ui/Theme.tsx"
+
+export default function App() {
+  return (
+    <Providers>
+      <Router />
+    </Providers>
+  )
+}
+
 const queryClient = new QueryClient()
 
-const App = () => {
+function Providers({ children }: React.PropsWithChildren) {
   return (
     <NetworkProvider>
       <QueryClientProvider client={queryClient}>
-        <Router>
-          <Box bg="linear-gradient(180deg, #525860 0%, #363A3F 100%)" px={6} height={"252px"} py={4}>
-            <Navbar />
-          </Box>
-
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/block/:blockId" element={<BlockDetailsPage />} />
-            <Route path="/transaction/:transactionId" element={<TransactionDetailsPage />} />
-            <Route path="/transaction/:transactionId/clause/:clauseIndex" element={<ClauseDetailsPage />} />
-            <Route path="/account/:address" element={<AccountPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Router>
+        <ChakraProvider value={config}>
+          <ColorModeProvider>{children}</ColorModeProvider>
+        </ChakraProvider>
       </QueryClientProvider>
     </NetworkProvider>
   )
 }
 
-export default App
+function Router() {
+  return (
+    <BrowserRouter>
+      <Box bg="linear-gradient(180deg, #525860 0%, #363A3F 100%)" px={6} height={"252px"} py={4}>
+        <Navbar />
+      </Box>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/block/:blockId" element={<BlockDetailsPage />} />
+        <Route path="/transaction/:transactionId" element={<TransactionDetailsPage />} />
+        <Route path="/transaction/:transactionId/clause/:clauseIndex" element={<ClauseDetailsPage />} />
+        <Route path="/account/:address" element={<AccountPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import "./utils/localization/i18n.ts"
-import { Provider } from "@/components/ui/Provider.tsx"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>,
 )
