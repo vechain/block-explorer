ac25d4b33c5a66ae93060b35e4e800f2b604bf1d
DavNej <david.nejar@gmail.com>
2025-07-16 12:55:36 +0200

    block transactions page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThorClientProvider } from "@/context/ThorClientProvider"
 import { DashboardPage } from "@/pages/dashboard/page"
 import { BlockPage } from "@/pages/block/page.tsx"
+import { BlockTransactionsPage } from "@/pages/block/transactions/page"
 import { TransactionPage } from "@/pages/transaction/page"
 import { ClauseDetailsPage } from "@/pages/ClauseDetailsPage.tsx"
 import { NotFoundPage } from "@/pages/NotFoundPage.tsx"
@@ -43,6 +44,7 @@ const Router = () => {
         <Route element={<Layout />}>
           <Route index element={<DashboardPage />} />
           <Route path="/block/:blockId" element={<BlockPage />} />
+          <Route path="/block/:blockId/transactions" element={<BlockTransactionsPage />} />
           <Route path="/transaction/:transactionId" element={<TransactionPage />} />
           <Route path="/transaction/:transactionId/clause/:clauseIndex" element={<ClauseDetailsPage />} />
           <Route path="/account/:address" element={<AccountPage />} />
