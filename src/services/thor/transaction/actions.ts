b1bb8b7cca652c2fc77e86c4b6b586a391c00ff3
DavNej <david.nejar@gmail.com>
2025-07-22 15:31:01 +0200

    Create type support for Legacy and Dyn Fees tx

--- src/services/thor/transaction/actions.ts
+++ src/services/thor/transaction/actions.ts
@@ -1,14 +1,53 @@
 import { Hex } from "@vechain/sdk-core"
 import { ThorClient, TransactionDetailNoRaw } from "@vechain/sdk-network"
 
+/**
+ * TODO: fix this transaction detail typing when the SDK is updated.
+ * Currently, there is no disctinction between dynamic fee and legacy transactions.
+ * for instance, "type" is not in TransactionDetailNoRaw.
+ * The verbosity in type definition is not ideal, but it's the best we can do for now.
+ */
+
+export type BaseTransaction = {
+  id: TransactionDetailNoRaw["id"]
+  chainTag: TransactionDetailNoRaw["chainTag"]
+  blockRef: TransactionDetailNoRaw["blockRef"]
+  expiration: TransactionDetailNoRaw["expiration"]
+  clauses: TransactionDetailNoRaw["clauses"]
+  gas: TransactionDetailNoRaw["gas"]
+  origin: TransactionDetailNoRaw["origin"]
+  meta: TransactionDetailNoRaw["meta"]
+  delegator: TransactionDetailNoRaw["delegator"]
+  nonce: TransactionDetailNoRaw["nonce"]
+  dependsOn: TransactionDetailNoRaw["dependsOn"]
+  size: TransactionDetailNoRaw["size"]
+}
+
+type DynamicFeeTransactionFields = {
+  type: 81
+  maxFeePerGas: string
+  maxPriorityFeePerGas: string
+}
+
+type LegacyTransactionFields = {
+  type: 0
+  gasPriceCoef: TransactionDetailNoRaw["gasPriceCoef"]
+}
+
+export type DynamicFeeTransaction = BaseTransaction & DynamicFeeTransactionFields
+export type LegacyTransaction = BaseTransaction & LegacyTransactionFields
+
+type TransactionDetail = DynamicFeeTransaction | LegacyTransaction
+
 export async function getTransaction({
   thorClient,
   transactionId,
 }: {
   thorClient: ThorClient
   transactionId: Hex
-}): Promise<TransactionDetailNoRaw | null> {
+}): Promise<TransactionDetail | null> {
   const tx = await thorClient.transactions.getTransaction(transactionId.toString())
 
-  return tx
+  return tx as TransactionDetail
+}
 }
