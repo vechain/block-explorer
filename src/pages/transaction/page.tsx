336be43f9791f14754a3b915a1ab685149200db9
DavNej <david.nejar@gmail.com>
2025-07-22 15:32:58 +0200

    update page

--- src/pages/transaction/page.tsx
+++ src/pages/transaction/page.tsx
@@ -1,131 +1,47 @@
 import { Navigate, useParams } from "react-router-dom"
 import { parseHex } from "@/utils/hex"
-import { useTransaction } from "@/services/thor/transaction/hooks"
+import { useTransaction, useTransactionReceipt } from "@/services/thor/transaction/hooks"
 import { Hex } from "@vechain/sdk-core"
-import { Stack, Table } from "@chakra-ui/react"
-import { AddressLink, BlockLink, TransactionClausesLink, TransactionLink } from "@/components/ui/Links"
+import { Flex, Stack } from "@chakra-ui/react"
 import { Subtitle, Title } from "@/components/ui/Typography"
-import { formatDateFromTimestamp } from "@/utils/date"
+import { CopyToClipBoard } from "@/components/ui/CopyToClipBoard"
+import { DynamicFeeTransaction } from "./components/DynamicFeeTransaction"
+import { LegacyTransaction } from "./components/LegacyTransaction"
 
 export const TransactionPage = () => {
   const { transactionId } = useParams<{ transactionId: string }>()
-
   const id = parseHex(transactionId)
 
-  if (!id) {
-    return <Navigate to="/404" replace state={{ message: "Invalid transaction id" }} />
-  }
-
-  return <TransactionDetails id={id} />
+  return id ? (
+    <TransactionDetails id={id} />
+  ) : (
+    <Navigate to="/404" replace state={{ message: "Invalid transaction id" }} />
+  )
 }
 
 const TransactionDetails = ({ id }: { id: Hex }) => {
   const { data: transaction, isLoading } = useTransaction(id)
+  const { data: receipt, isLoading: isReceiptLoading } = useTransactionReceipt(id)
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading || isReceiptLoading) return <div>Loading...</div>
   if (!transaction)
     return <Navigate to="/404" replace state={{ message: "The transaction you are looking for does not exist" }} />
 
-  const items = [
-    {
-      name: "ID",
-      value: transaction.id,
-    },
-    {
-      name: "Block Number",
-      value: <BlockLink blockId={transaction.meta.blockID}>#{transaction.meta.blockNumber}</BlockLink>,
-    },
-    {
-      name: "Block ID",
-      value: <BlockLink blockId={transaction.meta.blockID}>{transaction.meta.blockID}</BlockLink>,
-    },
-    {
-      name: "Timestamp",
-      value: formatDateFromTimestamp(transaction.meta.blockTimestamp),
-    },
-    {
-      name: "Origin",
-      value: <AddressLink address={transaction.origin} />,
-    },
-    {
-      name: "Delegator",
-      value: transaction.delegator ? <AddressLink address={transaction.delegator} /> : "None",
-    },
-    {
-      name: "Clauses",
-      value: (
-        <TransactionClausesLink transactionId={transaction.id}>
-          {transaction.clauses.length} Clauses
-        </TransactionClausesLink>
-      ),
-    },
-
-    {
-      name: "Type",
-      // TODO: fix this 👇
-      // @ts-expect-error - baseFeePerGas is not in the type
-      value: transaction.type.toString(),
-    },
-    {
-      name: "Chain Tag",
-      value: transaction.chainTag.toString(),
-    },
-    {
-      name: "Block Ref",
-      value: transaction.blockRef,
-    },
-    {
-      name: "Expiration",
-      value: transaction.expiration.toString(),
-    },
-    {
-      name: "Gas",
-      value: transaction.gas,
-    },
-    {
-      name: "Max Fee Per Gas",
-      // TODO: fix this 👇
-      // @ts-expect-error - baseFeePerGas is not in the type
-      value: transaction.maxFeePerGas,
-    },
-    {
-      name: "Max Priority Fee Per Gas",
-      // TODO: fix this 👇
-      // @ts-expect-error - baseFeePerGas is not in the type
-      value: transaction.maxPriorityFeePerGas,
-    },
-    {
-      name: "Nonce",
-      value: transaction.nonce,
-    },
-    {
-      name: "Depends On",
-      value: transaction.dependsOn ? (
-        <TransactionLink transactionId={transaction.dependsOn}>{transaction.dependsOn}</TransactionLink>
-      ) : (
-        "None"
-      ),
-    },
-    { name: "Size", value: transaction.size.toString() },
-  ]
+  if (!receipt) return <div>Transaction is pending</div>
 
   return (
     <Stack>
       <Title>Transaction details</Title>
-      <Subtitle>{transaction.id.toString()}</Subtitle>
+      <Flex alignItems="center" gap={2}>
+        <Subtitle>{transaction.id.toString()}</Subtitle>
+        <CopyToClipBoard value={transaction.id.toString()} />
+      </Flex>
 
-      <Table.ScrollArea my={12} borderWidth="1px" rounded="md">
-        <Table.Root size="md">
-          <Table.Body>
-            {items.map(item => (
-              <Table.Row key={item.name}>
-                <Table.Cell>{item.name}</Table.Cell>
-                <Table.Cell>{item.value}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
-      </Table.ScrollArea>
+      {transaction.type === 81 ? (
+        <DynamicFeeTransaction tx={transaction} receipt={receipt} />
+      ) : (
+        <LegacyTransaction tx={transaction} receipt={receipt} />
+      )}
     </Stack>
   )
 }
