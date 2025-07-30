f27881c7f179fec608329e24e9be4f87d8733666
DavNej <david.nejar@gmail.com>
2025-07-30 11:21:56 +0200

    remove unused hook

--- src/services/thor/vns/hooks.ts
+++ src/services/thor/vns/hooks.ts
@@ -1,18 +1,8 @@
 import { useQuery } from "@tanstack/react-query"
-import { getVnsAddress, getVnsName } from "./actions"
+import { getVnsName } from "./actions"
 import { useThorClient } from "../thor-client"
 import { Address } from "@vechain/sdk-core"
 import { parseAddress } from "@/utils/address"
-
-export function useVnsAddress(name: string) {
-  const { thorClient, activeNetwork } = useThorClient()
-
-  return useQuery({
-    queryKey: [getVnsAddress.name, name, activeNetwork.name],
-    queryFn: () => getVnsAddress({ thorClient, networkName: activeNetwork.name, name }),
-    staleTime: Infinity,
-  })
-}
 
 export function useVnsName(address: Address | string) {
   const { thorClient, activeNetwork } = useThorClient()
