e7683b7d35bde85628b27f19c37d69a3256e1cb6
DavNej <david.nejar@gmail.com>
2025-07-31 11:23:02 +0200

    TokenBalance component now supports HexString

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,17 +1,9 @@
-import { Heading, Text } from "@chakra-ui/react"
+import { Heading, HeadingProps, Text, TextProps } from "@chakra-ui/react"
 
-export const Title = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Heading as="h1" size="2xl" fontWeight="bold" color="fg">
-      {children}
-    </Heading>
-  )
+export const Title = (props: HeadingProps) => {
+  return <Heading as="h1" size="2xl" fontWeight="bold" color="fg" {...props} />
 }
 
-export const Subtitle = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Text fontSize="md" color="fg.muted">
-      {children}
-    </Text>
-  )
+export const Subtitle = (props: TextProps) => {
+  return <Text fontSize="md" color="fg.muted" {...props} />
 }
