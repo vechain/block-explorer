ac483661a01b9f67ce1cc2bf077df58ba7e22940
DavNej <david.nejar@gmail.com>
2025-09-03 14:08:48 +0200

    fix CORS for OPTION

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,17 @@ import { i18nRouter } from 'next-i18n-router'
 import { i18nConfig } from '@/i18n/config'
 
 export function middleware(request: NextRequest) {
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    })
+  }
+
   return i18nRouter(request, i18nConfig)
 }
 
