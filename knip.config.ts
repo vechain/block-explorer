c3afdd779ac7937b38c50bb18f32246004626fb5
DavNej <david.nejar@gmail.com>
2025-09-09 17:53:53 +0200

    fix build

--- knip.config.ts
+++ knip.config.ts
@@ -1,5 +1,5 @@
 export default {
   $schema: 'https://unpkg.com/knip@5/schema.json',
   project: ['**/*.ts', '**/*.tsx'],
-  ignore: ['**/*.spec.ts', '**/*.spec.tsx'],
+  ignore: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.tsx', 'lib/constants/stargate-nft.ts'],
 }
