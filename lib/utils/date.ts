46a505db76deb6526031a806e0b190dbd3418f27
DavNej <david.nejar@gmail.com>
2025-09-16 18:32:51 +0200

    create chart

--- lib/utils/date.ts
+++ lib/utils/date.ts
@@ -9,3 +9,24 @@ export const formatDateFromTimestamp = (timestamp: number) => {
     timeZone: 'UTC',
   })
 }
+
+export function timeFormat(timestamp: number): string {
+  //* format timestamp to HH:MM:SS
+  const date = new Date(timestamp * 1000)
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${hours}:${minutes}:${seconds}`
+}
+
+export function datetimeFormat(timestamp: number): string {
+  //* format timestamp to YYYY-MM-DD HH:MM:SS
+  const date = new Date(timestamp * 1000)
+  const year = String(date.getFullYear()).padStart(4, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
