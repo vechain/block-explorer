76ff70485a9af8dff23638fd4161b2c5907edbe3
Dougal Rea <dougal.rea@gmail.com>
2025-11-14 18:45:05 +0000

    validation records

--- terraform/frontend/route53.tf
+++ terraform/frontend/route53.tf
@@ -16,3 +16,12 @@ resource "aws_route53_record" "prod_frontend" {
     }
     depends_on = [aws_apprunner_service.frontend]
 }
+
+resource "aws_route53_record" "validation_records" {
+  count    = length([local.env.domain]) + 1
+  name     = tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].name
+  type     = tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].type
+  records  = [tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].value]
+  ttl      = 300
+  zone_id  = local.env.environment == "prod" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
+}
