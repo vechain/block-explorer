cf4bdad2edc22459ef487d34b0cb3f3a9bc6f57d
Dougal Rea <dougal.rea@gmail.com>
2025-11-14 17:28:24 +0000

    Update route53.tf

--- terraform/frontend/route53.tf
+++ terraform/frontend/route53.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "frontend" {
     count = local.env.enable_custom_domain ? 1 : 0
     zone_id = local.env.environment == "prod" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
     name    = local.env.domain
-    type    = "CNAME"
+    type    = local.env.environment == "prod" ? "A" : "CNAME"
     ttl     = 30
     records = [aws_apprunner_service.frontend.service_url]
 
