1875419fe83537d8a1173b252b23e9ef865d9336
Dougal Rea <dougal.rea@gmail.com>
2025-11-14 17:35:56 +0000

    Update route53.tf

--- terraform/frontend/route53.tf
+++ terraform/frontend/route53.tf
@@ -2,12 +2,25 @@
 # Route53 DNS Records
 ################################################################################
 
-resource "aws_route53_record" "frontend" {
-    count = local.env.enable_custom_domain ? 1 : 0
-    zone_id = local.env.environment == "prod" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
+resource "aws_route53_record" "preview_frontend" {
+    count = local.env.environment == "preview" ? 1 : 0
+    zone_id = data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
     name    = local.env.domain
-    type    = local.env.environment == "prod" ? "A" : "CNAME"
+    type    = "CNAME"
     ttl     = 30
     records = [aws_apprunner_service.frontend.service_url]
+    depends_on = [aws_apprunner_service.frontend]
+}
+
+resource "aws_route53_record" "prod_frontend" {
+    count = local.env.environment == "prod" ? 1 : 0
+    zone_id = data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id
+    name    = local.env.domain
+    type    = "A"
+    alias {
+        name    = aws_apprunner_service.frontend.service_url
+        zone_id = data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id
+        evaluate_target_health = true
+    }
     depends_on = [aws_apprunner_service.frontend]
 }
