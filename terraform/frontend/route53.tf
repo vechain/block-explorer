61d1cd3a48f4602359232aace5898d30694f50fc
Dougal Rea <73436291+dougalrea@users.noreply.github.com>
2025-11-14 18:51:48 +0000

    Production Deployment AWS (#146)
    * Production Deployment AWS
    
    * prod
    
    * Update route53.tf
    
    * Update prod.yaml
    
    * Update route53.tf
    
    * Update route53.tf
    
    * Update prod.yaml
    
    * Update route53.tf
    
    * Update route53.tf
    
    * Update route53.tf
    
    * validation records
    
    * Update route53.tf

--- terraform/frontend/route53.tf
+++ terraform/frontend/route53.tf
@@ -2,13 +2,26 @@
 # Route53 DNS Records
 ################################################################################
 
-resource "aws_route53_record" "frontend" {
-    count = local.env.enable_custom_domain ? 1 : 0
-    zone_id = local.env.environment == "production" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
-    name    = local.env.domain
-    type    = "CNAME"
-    ttl     = 30
-    records = [aws_apprunner_service.frontend.service_url]
+data "aws_apprunner_hosted_zone_id" "main" {}
 
+resource "aws_route53_record" "prod_frontend" {
+    count = local.env.enable_custom_domain ? 1 : 0
+    zone_id = local.env.environment == "prod" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
+    name    = local.env.domain
+    type    = "A"
+    alias {
+        name    = aws_apprunner_service.frontend.service_url
+        zone_id = data.aws_apprunner_hosted_zone_id.main.id
+        evaluate_target_health = true
+    }
     depends_on = [aws_apprunner_service.frontend]
+}
+
+resource "aws_route53_record" "validation_records" {
+  count    = length([local.env.domain]) + 1
+  name     = tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].name
+  type     = tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].type
+  records  = [tolist(aws_apprunner_custom_domain_association.frontend[0].certificate_validation_records)[count.index].value]
+  ttl      = 30
+  zone_id  = local.env.environment == "prod" ? data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_prod_id : data.terraform_remote_state.account_level.outputs.block_explorer_public_zone_preview_id
 }
