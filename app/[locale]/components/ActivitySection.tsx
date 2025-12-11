9062f015f3a067bd96abb9325a55d98cebfa7b44
Sebastian Seijo <sebastian.seijo@vechain.org>
2025-12-11 11:50:42 +0100

    add tx/clause to activity component table (#187)

--- app/[locale]/components/ActivitySection.tsx
+++ app/[locale]/components/ActivitySection.tsx
@@ -3,13 +3,13 @@
 import { Heading } from '@chakra-ui/react'
 import { Surface } from '@/components/ui/Surface'
 import { TableSkeleton } from '@/components/ui/Table'
-import { useLatestBlocksCompressed } from '@/services/thor/hooks'
+import { useLatestBlocksExpanded } from '@/services/thor/hooks'
 import { BlocksTable } from './BlocksTable'
 
 const BLOCKS_TO_DISPLAY = 5
 
 export const ActivitySection = () => {
-  const { data: latestBlocks, isPending } = useLatestBlocksCompressed({ count: BLOCKS_TO_DISPLAY })
+  const { data: latestBlocks, isPending } = useLatestBlocksExpanded({ count: BLOCKS_TO_DISPLAY })
 
   return (
     <Surface>
