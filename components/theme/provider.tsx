517dec7af4a9246a1bdbd6f6d0a239dee48d3af3
Sebastian Seijo <sebastian.seijo@vechain.org>
2025-12-15 11:53:15 +0100

    modify theme , no light mode

--- components/theme/provider.tsx
+++ components/theme/provider.tsx
@@ -2,12 +2,12 @@
 
 import { ChakraProvider as ChakraProviderBase } from '@chakra-ui/react'
 import { ThemeProvider } from 'next-themes'
-import { system } from './config'
+import { ColorMode, system } from './config'
 
 export const ChakraProvider = ({ children }: React.PropsWithChildren) => {
   return (
     <ChakraProviderBase value={system}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme={ColorMode.DARK} enableSystem={false} disableTransitionOnChange>
         {children}
       </ThemeProvider>
     </ChakraProviderBase>
