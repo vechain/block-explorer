3d7b6456a5f4110695baae7e11a87bf00a4870c0
Davide Carpini <carpini.davide@gmail.com>
2025-12-16 12:10:11 +0100

    support center translated

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -64,7 +64,7 @@ export const Footer = () => {
                   {'VeVote'} <FiExternalLink />
                 </Link>
                 <Link href="https://support.vechain.org/" {...linkProps}>
-                  {'Support Center'} <FiExternalLink />
+                  {t('Support Center')} <FiExternalLink />
                 </Link>
               </VStack>
             </HStack>
