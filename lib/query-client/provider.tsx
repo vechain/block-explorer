9423975e437c66309085f29bc233532306e2186b
Davide Carpini <carpini.davide@gmail.com>
2025-12-22 10:53:14 +0100

    fix: remove use state

--- lib/query-client/provider.tsx
+++ lib/query-client/provider.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { QueryClientProvider as TanstackQueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { useState } from 'react'
 import { getQueryClient } from '@/lib/query-client/query-client'
 
 export const QueryClientProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => getQueryClient())
+  const queryClient = getQueryClient()
 
   return (
     <TanstackQueryClientProvider client={queryClient}>
