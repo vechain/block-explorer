bc7614055a1335681ae1f659158e1a8507227728
Sebastian Seijo <sebastian.seijo@vechain.org>
2026-01-02 03:09:34 +0100

    remove previous component logic non-design related

--- app/[locale]/address/[address]/components/details/AccountDetails.tsx
+++ app/[locale]/address/[address]/components/details/AccountDetails.tsx
@@ -1,90 +1,17 @@
 'use client'
 
-import { Stack, Table, Tabs } from '@chakra-ui/react'
-import { useTranslation } from 'react-i18next'
-import { LuArrowLeftRight, LuCoins, LuImage } from 'react-icons/lu'
-import { TbTransfer } from 'react-icons/tb'
-import { VETBalance, VTHOBalance } from '@/components/ui-legacy/TokenBalance'
-import { VnsBadge } from '@/components/ui-legacy/VnsBadge'
-import { useTabs } from '@/hooks/useTabs'
+import { Stack } from '@chakra-ui/react'
 import type { Account } from '@/lib/schemas'
-import { useVnsName } from '@/services/thor/hooks'
-import { AccountNftsTab } from '../tabs/AccountNftTab'
-import { AccountTokensTab } from '../tabs/AccountTokensTab'
-import { AccountTransactionsTab } from '../tabs/AccountTransactionsTab'
-import { AccountTransfersTab } from '../tabs/AccountTransfersTab'
 import { AccountNftsSection } from '../sections/AccountNftsSection'
 import { AccountSummary } from './AccountSummary'
 import { AccountTransactionsSection } from '../sections/AccountTransactionsSection'
 
 export const AccountDetails = ({ account }: { account: Account }) => {
-  const { t } = useTranslation()
-  const { data: vnsName } = useVnsName(account.address)
-  const { currentTab, handleTabChange } = useTabs('transactions')
-
-  const items = [
-    {
-      name: t('VNS'),
-      value: <VnsBadge size="md" address={account.address} vnsName={vnsName} />,
-    },
-    { name: t('Balance'), value: <VETBalance balance={account.vet} /> },
-    { name: t('VTHO / Energy'), value: <VTHOBalance balance={account.vtho} /> },
-  ]
-
   return (
     <Stack flex={1} gap="8">
       <AccountSummary address={account.address} />
       <AccountTransactionsSection address={account.address} />
       <AccountNftsSection address={account.address} />
-
-      <Table.ScrollArea my={12} borderWidth="1px" rounded="md">
-        <Table.Root size="md">
-          <Table.Body>
-            {items.map(item => (
-              <Table.Row key={item.name} bg="bg-secondary">
-                <Table.Cell>{item.name}</Table.Cell>
-                <Table.Cell>{item.value}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
-      </Table.ScrollArea>
-
-      <Tabs.Root value={currentTab} onValueChange={handleTabChange} variant="subtle" lazyMount>
-        <Tabs.List bg="bg.muted" rounded="l3">
-          <Tabs.Trigger value="transactions">
-            <LuArrowLeftRight />
-            {t('Transactions')}
-          </Tabs.Trigger>
-          <Tabs.Trigger value="transfers">
-            <TbTransfer />
-            {t('Transfers')}
-          </Tabs.Trigger>
-          <Tabs.Trigger value="tokens">
-            <LuCoins />
-            {t('Tokens')}
-          </Tabs.Trigger>
-          <Tabs.Indicator rounded="l3" />
-          <Tabs.Trigger value="nfts">
-            <LuImage />
-            {t('NFTs')}
-          </Tabs.Trigger>
-          <Tabs.Indicator rounded="l3" />
-        </Tabs.List>
-
-        <Tabs.Content value="transactions">
-          <AccountTransactionsTab address={account.address} />
-        </Tabs.Content>
-        <Tabs.Content value="transfers">
-          <AccountTransfersTab address={account.address} />
-        </Tabs.Content>
-        <Tabs.Content value="tokens">
-          <AccountTokensTab address={account.address} />
-        </Tabs.Content>
-        <Tabs.Content value="nfts">
-          <AccountNftsTab address={account.address} />
-        </Tabs.Content>
-      </Tabs.Root>
     </Stack>
   )
 }
