eff38ecba8468f7ea3ec8e29ef7bed377a205fe6
DavNej <david.nejar@gmail.com>
2026-01-02 15:04:38 +0100

    chore: use thor vns utils to retrieve vns

--- services/thor/vns.ts
+++ services/thor/vns.ts
@@ -1,23 +1,10 @@
 import { queryOptions, skipToken } from '@tanstack/react-query'
-import { ABIFunction } from '@vechain/sdk-core'
-import { ThorClient } from '@vechain/sdk-network'
-import { z } from 'zod'
-import { NETWORKS, NetworkName } from '@/lib/constants/network'
+import { vnsUtils } from '@vechain/sdk-network'
+import { NetworkName } from '@/lib/constants/network'
 import { type AddressString, addressStringSchema } from '@/lib/schemas'
 import { isZeroAddress } from '@/lib/utils/address'
 import { normalizeName } from '@/lib/utils/vns'
-import { zodParse } from '@/lib/utils/zod'
-
-const VNS_RESOLVER: Partial<{ [key in NetworkName]: string }> = {
-  [NetworkName.MAINNET]: '0xA11413086e163e41901bb81fdc5617c975Fa5a1A',
-  [NetworkName.TESTNET]: '0xc403b8EA53F707d7d4de095f0A20bC491Cf2bc94',
-}
-
-const VNS_FUNCTION_ABI_GET_ADDRESSES = new ABIFunction(
-  'function getAddresses(string[] names) returns (address[] addresses)',
-)
-
-const VNS_FUNCTION_ABI_GET_NAMES = new ABIFunction('function getNames(address[] addresses) returns (string[] names)')
+import { getThorClient } from './client'
 
 const VNS_NAME_QUERY_KEY = 'getVnsName'
 
@@ -35,34 +22,15 @@ const getVnsName = async ({
   networkName: NetworkName
   address: AddressString
 }): Promise<string | null> => {
-  const vnsContractAddress = VNS_RESOLVER[networkName]
-  if (!vnsContractAddress) return null
+  const thorClient = getThorClient(networkName)
+  const vnsName = await vnsUtils.lookupAddress(thorClient, address)
 
-  const thorClient = ThorClient.at(NETWORKS[networkName].url)
-  const { result: vnsResult, success } = await thorClient.contracts.executeCall(
-    vnsContractAddress,
-    VNS_FUNCTION_ABI_GET_NAMES,
-    [[address]],
-  )
-
-  if (!success) {
+  if (!vnsName) {
     return null
   }
 
-  const result = zodParse({
-    data: vnsResult,
-    schema: vnsResultSchema,
-    errorMessage: 'Failed to parse VNS result',
-  })
-
-  const vnsName = result.array[0][0]
-
   return vnsName
 }
-
-const vnsResultSchema = z.object({
-  array: z.array(z.array(z.string())).nonempty(),
-})
 
 export const getVnsAddress = async ({
   networkName,
@@ -71,25 +39,14 @@ export const getVnsAddress = async ({
   networkName: NetworkName
   name: string
 }): Promise<AddressString | null> => {
-  const vnsContractAddress = VNS_RESOLVER[networkName]
-  if (!vnsContractAddress) return null
+  const thorClient = getThorClient(networkName)
 
-  const thorClient = ThorClient.at(NETWORKS[networkName].url)
-  const { result, success } = await thorClient.contracts.executeCall(
-    vnsContractAddress,
-    VNS_FUNCTION_ABI_GET_ADDRESSES,
-    [[normalizeName(name)]],
-  )
+  const resolvedAddress = await vnsUtils.resolveName(thorClient, normalizeName(name))
 
-  if (!success || !result.array || result.array.length === 0) {
+  if (!resolvedAddress) {
     return null
   }
-
-  const [address] = zodParse({
-    data: result.array[0],
-    schema: z.array(addressStringSchema),
-    errorMessage: 'Failed to parse VNS address',
-  })
+  const address = addressStringSchema.parse(resolvedAddress)
 
   if (isZeroAddress(address)) return null
 
