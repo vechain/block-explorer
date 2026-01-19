a9bcbfaf86557f44568de4329a7b7282fb867f9c
Davide Carpini <carpini.davide@gmail.com>
2026-01-19 12:01:10 +0100

    fix: not found layout (#388)

--- components/error/NotFound.tsx
+++ components/error/NotFound.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AbsoluteCenter, Box, Button, EmptyState, Text } from '@chakra-ui/react'
+import { Box, Button, EmptyState, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useTranslation } from 'react-i18next'
 import { AiOutlineStop } from 'react-icons/ai'
@@ -9,25 +9,21 @@ export const NotFound = ({ title, description }: { title?: string; description?:
   const { t } = useTranslation()
 
   return (
-    <Box position="fixed" bg="bg-primary" top="0" left="0" right="0" bottom="0">
-      <AbsoluteCenter>
-        <EmptyState.Root size="md">
-          <EmptyState.Content>
-            <EmptyState.Title>{title ?? t('This page does not exist')}</EmptyState.Title>
-
-            <EmptyState.Indicator>
-              <AiOutlineStop />
-            </EmptyState.Indicator>
-
-            <EmptyState.Description display="flex" flexDirection="column" alignItems="center" gap="8">
-              {description && <Text as="span">{description}</Text>}
-              <Button asChild variant="subtle" colorPalette="purple" size="sm">
-                <Link href="/">{t('Back to dashboard')}</Link>
-              </Button>
-            </EmptyState.Description>
-          </EmptyState.Content>
-        </EmptyState.Root>
-      </AbsoluteCenter>
+    <Box>
+      <EmptyState.Root size="md">
+        <EmptyState.Content>
+          <EmptyState.Indicator>
+            <AiOutlineStop />
+          </EmptyState.Indicator>
+          <EmptyState.Title>{title ?? t('This page does not exist')}</EmptyState.Title>
+          <EmptyState.Description display="flex" flexDirection="column" alignItems="center" gap="8">
+            {description && <Text as="span">{description}</Text>}
+            <Button asChild variant="subtle" colorPalette="purple" size="sm">
+              <Link href="/">{t('Back to dashboard')}</Link>
+            </Button>
+          </EmptyState.Description>
+        </EmptyState.Content>
+      </EmptyState.Root>
     </Box>
   )
 }
